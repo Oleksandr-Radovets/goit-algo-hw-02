ed276f417e4a25bddc462ab493c93035aeadefdc
sasha <radovietsoleksandr@gmail.com>
2025-01-08 19:13:49 +0200

    al_2

--- Task_1.py
+++ Task_1.py
@@ -1,4 +1,3 @@
-
 # Завдання 1.
 
 import queue
@@ -46,32 +45,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-# Завдання 2.
-
-from collections import deque
-
-def is_palindrome(s):
-    # Видаляємо пробіли, переводимо рядок у нижній регістр і залишаємо тільки алфавітні символи
-    clean_string = ''.join(char.lower() for char in s if char.isalnum())
-
-    # Створюємо двосторонню чергу
-    char_deque = deque(clean_string)
-
-    # Порівнюємо символи з обох кінців черги
-    while len(char_deque) > 1:
-        if char_deque.popleft() != char_deque.pop():
-            return False  # Не є паліндромом
-
-    return True  # Є паліндромом
-
-# Приклади використання
-
-print(is_palindrome("madam"))  # True
-print(is_palindrome("level"))  # True
-print(is_palindrome("noon"))   # True
-print(is_palindrome("A man, a plan, a canal: Panama"))  # True
-print(is_palindrome("racecar"))  # True
-print(is_palindrome("Hello"))  # False
